00e8169b35d91e98448c4e0ccfabddad0f143179
Banner <banner@fifthdomain.com.au>
2018-05-31 16:22:07 +1000

    making how to page

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,41 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
+
+const path = require('path')
+const versions = require('./versions.json')
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators
+
+  const postTemplate = path.resolve(`src/templates/postTemplate.js`)
+
+  return graphql(`
+    {
+      allMarkdownRemark(filter: { fileAbsolutePath: { regex: "/howto/" } }) {
+        edges {
+          node {
+            frontmatter {
+              path
+              version
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      let versionNumber = versions.indexOf(node.frontmatter.version) + 1
+      createPage({
+        path: `v${versionNumber}/howto/${node.frontmatter.path}`,
+        component: postTemplate,
+        context: { slug: node.frontmatter.path }, // additional data can be passed via context
+      })
+    })
+  })
+}
